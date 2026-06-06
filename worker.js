f428906415378ef398d0cdb8281dfc109215fdc7
Bangkibonih <kiting111205@gmail.com>
2026-06-06 12:46:28 +0700

    Add worker.js to fetch and return JavaScript content

---
+++ worker.js
@@ -0,0 +1,15 @@
+export default {
+  async fetch(request) {
+    const res = await fetch('https://quge5.com/88/tag.min.js', {
+      headers: { 'User-Agent': request.headers.get('User-Agent') }
+    });
+    const body = await res.text();
+    return new Response(body, {
+      headers: {
+        'Content-Type': 'application/javascript',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  }
+}
